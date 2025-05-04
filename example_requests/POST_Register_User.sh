0896fe85ecf6f72bae09b860e476b5fee02de5c0
Lucas Breda <mascarenhas-lblm@hotmail.com>
2025-05-03 21:36:51 -0300

    Update 03/05/2025

--- example_requests/POST_Register_User.sh
+++
@@ -1,9 +0,0 @@
-curl --request POST \
-  --url 'https://backtest.lucasbreda.me/users/register' \
-  --header 'Content-Type: application/json' \
-  --data '{
-    "username": "newuser",
-    "password": "securepassword123"
-    }'
-
-read -p "Pressione qualquer tecla para sair..."

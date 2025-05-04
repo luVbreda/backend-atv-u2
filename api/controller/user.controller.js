5538edad9077ec80569c7f53a2111c2dce997192
Lucas Breda <mascarenhas-lblm@hotmail.com>
2025-05-03 21:30:14 -0300

    Update 03/05/2025

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -5,33 +5,32 @@ import User from '../models/User.js';
 
 const register = async (req, res) => {
     console.log("Registering user:", req.body);
-    if (!req.body || !req.body.username || !req.body.password) {
-        return res.status(400).json({ message: 'Username and password are required' });
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+        return res.status(400).json({ message: 'Username, email, and password are required' });
     }
 
-    const { username, password } = req.body;
-
     try {
-        const savedUser = await createUser(username, password);
+        const savedUser = await createUser(username, email, password);
         console.log("Saved user:", savedUser);
-        return res.status(200).json({ message: 'User registered successfully' });
+        return res.status(201).json({ message: 'User registered successfully', user: savedUser });
     } catch (error) {
         console.error("Error saving user:", error);
-        return res.status(500).json({ message: `Error saving user ${error}` });
+        return res.status(500).json({ message: `Error saving user ${error.message}` });
     }
 };
 
 const login = async (req, res) => {
     console.log("Logging in user:", req.body);
+    const { email, password } = req.body;
 
-    if (!req.body || !req.body.username || !req.body.password) {
-        return res.status(400).json({ message: 'Username and password are required' });
+    if (!email || !password) {
+        return res.status(400).json({ message: 'Email and password are required' });
     }
 
-    const { username, password } = req.body;
-
     try {
-        const user = await User.findOne({ username }).select('+password');
+        const user = await User.findOne({ email }).select('+password');
         if (!user) {
             return res.status(404).json({ message: 'User not found' });
         }
@@ -41,10 +40,8 @@ const login = async (req, res) => {
             return res.status(401).json({ message: 'Invalid credentials' });
         }
 
-        // Gere o token JWT
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-        // Retorne o token na resposta
         return res.status(200).json({ message: 'Login successful', token });
     } catch (error) {
         console.error("Error logging in user", error);
